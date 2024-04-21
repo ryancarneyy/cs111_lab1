75b17f8169ab8bd270370915fd7ad0aa7ae69349
Ryan Carney <ryancarney771@gmail.com>
2024-04-20 20:20:03 -0700

    Added pycache to gitignore, fixed bug with more than 3 pipe arguments

--- pipe.c
+++ pipe.c
@@ -7,10 +7,8 @@
 #include <errno.h>
 
 int main(int argc, char *argv[])
-{
-	int (*pipes)[2] = malloc((argc - 2) * sizeof(*pipes));
-	pid_t *pids = malloc((argc - 1) * sizeof(*pids));
-	int *status = malloc((argc - 1) * sizeof(*status));
+{	
+
 
 	// checking number of arguments
 	if(argc == 1) {
@@ -22,12 +20,17 @@ int main(int argc, char *argv[])
 		return 0;
 	}
 
+	int (*pipes)[2] = malloc((argc - 2) * sizeof(*pipes));
+	pid_t *pids = malloc((argc - 1) * sizeof(*pids));
+	int *status = malloc((argc - 1) * sizeof(*status));
+
 	if(argc == 3) {
 		//TODO
+
 	}
 
 	if(pipe(pipes[0]) == -1) {
-		printf("Error opening pipe");
+		printf("Error opening pipe\n");
 		return 1;
 	}
 	
@@ -35,7 +38,7 @@ int main(int argc, char *argv[])
 	pids[0] = fork();
 
 	if( pids[0] == -1 ) {
-		printf("Error opening first child process");
+		printf("Error opening first child process\n");
 		return 1;
 	}
 	// if pid1 == 0 then child process running
@@ -45,7 +48,7 @@ int main(int argc, char *argv[])
 
 		// redirecting stdout of LHS to pipe's write
 		if(dup2(pipes[0][1], STDOUT_FILENO) == -1) {
-			printf("Error connecting ls's stdout to pipe's write");
+			printf("Error connecting ls's stdout to pipe's write\n");
 			return 1;
 		}
 		// closing pipe's write in child process (unneeded)
@@ -65,7 +68,7 @@ int main(int argc, char *argv[])
 			close(pipes[i-1][1]);
 
 			// wait for LHS of pipe to finish
-			waitpid(pids[i-1], &status[i-1], 0);
+			// waitpid(pids[i-1], &status[i-1], 0);
 
 			if( pipe(pipes[i]) == -1) {
 				printf("Error creating pipe %d\n", i);
@@ -80,7 +83,7 @@ int main(int argc, char *argv[])
 
 				// redirecting stdin of child to pipe's read
 				if(dup2(pipes[i-1][0], STDIN_FILENO) == -1) {
-					printf("Error redirecting %s's stdin to pipe %d read", argv[i], i-1);
+					printf("Error redirecting %s's stdin to pipe %d read\n", argv[i], i-1);
 					return 1;
 				}
 				// closing previous pipe's read in child process
@@ -88,54 +91,51 @@ int main(int argc, char *argv[])
 
 				// redirecting stdout of child to pipe's write
 				if(dup2(pipes[i][1], STDOUT_FILENO) == -1) {
-					printf("Error redirecting %s's stdout to pipe %d's write", argv[i], i);
+					printf("Error redirecting %s's stdout to pipe %d's write\n", argv[i], i);
 					return 1;
 				}
 				// close current pipe's write in child process
 				close(pipes[i][1]);
-				system(argv[2]);
-				return 0;
-			}
-			else {
-
-				// wait for child process 2 to finish
-				waitpid(pids[argc-3], &status[argc-3], 0);
-
-				const int SECONDLASTPIPE = argc-4;
-				const int LASTPIPE = argc-3;
-				const int LASTPID = argc-2;
-
-				// close second to last pipe's read in parent process
-				close(pipes[SECONDLASTPIPE][0]);
-				// close previous pipe's write in parent process
-				close(pipes[LASTPIPE][1]);
-
-				pids[LASTPID] = fork();
-
-				if( pids[LASTPID] == -1 ) {
-					printf("Error opening final process");
-					return 1;
-				}
-				else if( pids[LASTPID] == 0) {
-					if(dup2(pipes[LASTPIPE][0], STDIN_FILENO) == -1) {
-						printf("Error redirecting last process's stdin to last pipe's read");
-						return 1;
-					}
-					// close last pipe's read in child process
-					close(pipes[LASTPIPE][0]);
-
-					system(argv[argc-1]);
-					return 0;
-				}
-				else {
-					// close last pipe's read in parent process
-					waitpid(pids[LASTPID], &status[LASTPID], 0);
-					close(pipes[LASTPIPE][0]);
-				}
-
+				system(argv[i+1]);
 				return 0;
 			}
 		}
+
+		// wait for previous process to finish
+		waitpid(pids[argc-2], &status[argc-2], 0);
+
+		const int SECONDLASTPIPE = argc-4;
+		const int LASTPIPE = argc-3;
+		const int LASTPID = argc-2;
+
+		// close second to last pipe's read in parent process
+		close(pipes[SECONDLASTPIPE][0]);
+		// close previous pipe's write in parent process
+		close(pipes[LASTPIPE][1]);
+
+		pids[LASTPID] = fork();
+		if( pids[LASTPID] == -1 ) {
+			printf("Error opening final process\n");
+			return 1;
+		}
+		else if( pids[LASTPID] == 0 ) {
+			if(dup2(pipes[LASTPIPE][0], STDIN_FILENO) == -1) {
+				perror("Error redirecting last process's stdin to last pipe's read");
+				return 1;
+			}
+			// close last pipe's read in child process
+			close(pipes[LASTPIPE][0]);
+
+			system(argv[argc-1]);
+			return 0;
+		}
+		else {
+			// close last pipe's read in parent process
+			waitpid(pids[LASTPID], &status[LASTPID], 0);
+			close(pipes[LASTPIPE][0]);
+		}
+
+		return 0;
 	}
 
 }
